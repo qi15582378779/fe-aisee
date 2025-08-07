953af028ddf7166fd4aedaf22104526ba37fe4e5
zhangyuqi <qi1194085859@gmail.com>
2025-08-07 18:08:11 +0800

    Update next.config.ts to configure output settings, enable trailing slashes, and set images to unoptimized.

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
     // 在构建时忽略 TypeScript 错误
     ignoreBuildErrors: true,
   },
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
